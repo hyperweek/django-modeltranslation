37ec8954ee7e1a2e9e22b66999be6b693361230c
zyegfryed <zyegfryed@gmail.com>
2011-09-19 15:58:06 +0200

    Remobed useless TRANSLATION_REGISTRY settings.

--- modeltranslation/settings.py
+++ modeltranslation/settings.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
-from warnings import warn
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-
-if hasattr(settings, 'MODELTRANSLATION_TRANSLATION_REGISTRY'):
-    TRANSLATION_REGISTRY =\
-    getattr(settings, 'MODELTRANSLATION_TRANSLATION_REGISTRY', None)
-elif hasattr(settings, 'TRANSLATION_REGISTRY'):
-    warn('The setting TRANSLATION_REGISTRY is deprecated, use '
-         'MODELTRANSLATION_TRANSLATION_REGISTRY instead.', DeprecationWarning)
-    TRANSLATION_REGISTRY = getattr(settings, 'TRANSLATION_REGISTRY', None)
-else:
-    raise ImproperlyConfigured("You haven't set the "
-                               "MODELTRANSLATION_TRANSLATION_REGISTRY "
-                               "setting yet.")
 
 AVAILABLE_LANGUAGES = [l[0] for l in settings.LANGUAGES]
 DEFAULT_LANGUAGE = getattr(settings, 'MODELTRANSLATION_DEFAULT_LANGUAGE', None)
